525b8660b7f30036b808e10696f08e3e1e22ed63
Kenno-Warise <wariken0523@gmail.com>
2025-05-16 18:56:26 +0900

    SSLに対応した設定を行う

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -11,4 +11,9 @@ docker network create mynetwork
 docker run --name backend -v ./mysite:/usr/share/nginx/html --network mynetwork -d nginx:latest
 
 # Nginxの設定ファイルを元にリバースプロキシを実行
-docker run --name nginx -v ./conf.d/default.conf:/etc/nginx/conf.d/default.conf --network mynetwork -d -p 80:80 nginx:latest
+docker run --name nginx \
+  -v ./ssl:/etc/ssl \
+  -v ./conf.d/default.conf:/etc/nginx/conf.d/default.conf \
+  --network mynetwork \
+  -d -p 80:80 -p 443:443 \
+  nginx:latest
