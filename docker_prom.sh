b3fc95a2d4ba4b42838f138c1604bcbdad83ec21
Kenno-Warise <wariken0523@gmail.com>
2025-05-17 17:14:51 +0900

    YAML設定ファイルを作成してコンテナを構築

---
+++ docker_prom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run docker container nginx
+# 2025/5/17
+# 実行コマンド
+# 「./docker_prom.sh」
+
+# Prometheusコンテナ
+docker run --name prometheus \
+  -v ./prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+  -d -p 127.0.0.1:9090:9090 \
+  prom/prometheus
+
